32a0b2fbf07c58e983818de4a5566014db0d274f
Tabuke Ezekiel <tabukeezekiel9@gmail.com>
2025-07-07 22:06:25 +0100

    STRIPE CHECK OUT

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-// tabukeezekiel9
-// 8nA2Siyxl5mqZ6tG
-
 import cookieParser from "cookie-parser";
 import express from "express";
 import authRoute from "./routes/auth.routes.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
-import { PORT } from "./config/env.js";
+import { PORT, STRIPE_SECRET_KEY, YOUR_DOMAIN } from "./config/env.js";
 import { connectDb } from "./database/mongodb.js";
 import authorize from "./middlewares/auth.middleware.js";
 import cors from "cors";
+import stripeRoute from "./routes/stripe.routes.js";
 
 const app = express();
+app.use(express.static("public"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -18,12 +17,9 @@ app.use(cors());
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/checkout", stripeRoute);
 app.use(authorize);
 app.use(errorMiddleware);
-
-app.get("/", (req, res) => {
-  res.send("Hello World!!!!!");
-});
 
 app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
---
+++ routes/stripe.routes.js
@@ -0,0 +1,24 @@
+import express, { Router } from "express";
+import { STRIPE_SECRET_KEY } from "../config/env";
+
+const stripeRoute = Router();
+const stripe = require("stripe")(STRIPE_SECRET_KEY);
+
+stripeRoute.post("/create-checkout-session", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+        price: "{{PRICE_ID}}",
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+  });
+
+  res.redirect(303, session.url);
+});
+
+export default stripeRoute;
